f87df4c5541033557c5abc444f81671dd7f6bfd5
Renuka <renukaullegaddi03@gmail.com>
2026-08-08 12:32:59 +0530

    Login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,30 @@
-import Image from "next/image";
+import { Pyramid } from "lucide-react"
 
-export default function Home() {
+export default function Home(){
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="flex flex-col items-center text-center">
+      <div className="flex items-center gap-2 mt-[30vh]">
+        <Pyramid size={18} className="bg-black text-white rounded-[4px] p-1"/>
+       <p className="text-sm">pyramid</p>
+      </div>
+
+      <div className="w-[320px] mt-5">
+        <p className="text-lg font-semibold">Let's get back on track</p>
+        <p className="text-gray-500 text-xs flex justify-center mt-1">Enter your email below to login to your account</p>
+        <button className="w-full h-9 rounded-full bg-black text-white mt-5">
+           Continue as Guest
+        </button>
+        <button className="w-full h-9 rounded-full border border-gray-200 mt-2 flex items-center justify-center gap-2">
+          <span className="font-semibold">G</span>
+          Login with Google
+        </button>
+      </div>
+
+     <div className="w-[180px] mt-5 text-center">
+  <p className="text-xs text-gray-500">
+    By clicking continue, you agree to our{" "}<span className="underline">Terms of Service</span> and{" "}<span className="underline">Privacy Policy</span> 
+  </p>
+</div>
+    </main>
   );
 }
