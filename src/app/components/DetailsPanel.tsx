f4bd9829ef89eb9d4ae8ccea90c3c88f568394c3
Renuka <renukaullegaddi03@gmail.com>
2026-08-11 15:05:30 +0530

    completed priority dropdown in details panel

--- src/app/components/DetailsPanel.tsx
+++ src/app/components/DetailsPanel.tsx
@@ -1,33 +1,34 @@
 "use client";
 import CalendarPicker from "./CalendarPicker";
 import { useState } from "react";
-import { Settings,ChevronDown, ChevronUp } from "lucide-react";
+import { Settings, ChevronDown, ChevronUp } from "lucide-react";
+import PriorityDropdown from "./PriorityDropdown";
 export default function DetailsPanel() {
     const [isOpen, setIsOpen] = useState(true)
 
     return (
 
         <aside className="w-[260px] border rounded-lg px-5 py-6 ml-5 mb-20 h-fit">
-           <div className="flex items-center justify-between">
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="flex items-center justify-between"
-            >
-                <span>{isOpen ? <ChevronUp /> : <ChevronDown />}</span>
-                <h2 className="text-sm font-semibold">Details</h2>
-                
-            </button>
+            <div className="flex items-center justify-between">
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="flex items-center justify-between"
+                >
+                    <span>{isOpen ? <ChevronUp /> : <ChevronDown />}</span>
+                    <h2 className="text-sm font-semibold">Details</h2>
 
-              <div className="flex items-center gap-3">
-                <button>
-                    +
                 </button>
 
-                <button>
-                  <Settings size={16} />
-                </button>
-              </div>
-             </div>
+                <div className="flex items-center gap-3">
+                    <button>
+                        +
+                    </button>
+
+                    <button>
+                        <Settings size={16} />
+                    </button>
+                </div>
+            </div>
             {isOpen && (
                 <div className="mt-6 space-y-5">
 
@@ -38,10 +39,10 @@ export default function DetailsPanel() {
 
                     <div className="flex items-center gap-15">
                         <p className="text-xs text-gray-400">Priority</p>
-                        <p className="mt-1 text-sm">High</p>
+                        <PriorityDropdown />
                     </div>
 
-                    <div className="flex items-center gap-15">
+                    <div className="flex items-center gap-12">
                         <p className="text-xs text-gray-400">Members</p>
                         <p className="mt-1 text-sm">CN</p>
                     </div>
@@ -59,7 +60,7 @@ export default function DetailsPanel() {
                         </div>
                     </div>
 
-                    <div className="flex items-center gap-15">
+                    <div className="flex items-center gap-14">
                         <p className="text-xs text-gray-400">Labels</p>
                         <p className="mt-1 text-sm">Research</p>
                     </div>
@@ -69,7 +70,7 @@ export default function DetailsPanel() {
                         <p className="mt-1 text-sm">Development</p>
                     </div>
 
-                    <div className="flex items-center gap-15">
+                    <div className="flex items-center gap-12">
                         <p className="text-xs text-gray-400">Reports</p>
                         <p className="mt-1 text-sm">No reports</p>
                     </div>
---
+++ src/app/components/PriorityDropdown.tsx
@@ -0,0 +1,63 @@
+"use client"
+import { Check, SignalMedium } from "lucide-react";
+import { useState } from "react"
+
+export default function PriorityDropdown() {
+    const [priority, setPriority] = useState("Urgent");
+    const [isOpen, setIsOpen] = useState(false);
+
+    const priorities = ["No priority", "Urgent", "High","Medium", "Low"];
+    const priorityColors: Record<string, string>={
+      "No priority": "text-black",
+      "Urgent": "text-red-500",
+      "High": "text-orange-500",
+      "Medium": "text-yellow-500",
+      "Low": "text-gray-500",
+    }
+    return (
+        <div className="relative">
+            <p className="text-xs text-gray-400"></p>
+
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className={`mt-1 text-sm ${priorityColors[priority]}`}
+            >
+                {priority}
+            </button>
+
+            {isOpen && (
+                <div className="absolute left-0 top-full z-10 mt-2 w-[220px] rounded-lg bg-white p-2 shadow-sm">
+                    <p className="px-2 py-1 text-xs text-gray-400">
+                        Priority
+                    </p>
+
+                    {priorities.map((item) => (
+                        <button
+                            key={item}
+                            onClick={() => {
+                                setPriority(item);
+                                setIsOpen(false);
+                            }}
+                            className="flex w-full items-center justify-between rounded-md px-2 py-2 text-sm hover:bg-gray-50"
+                        >
+
+                            <div className="flex items-center gap-2">
+                                {item!=="No Priority" && (
+                                    <SignalMedium 
+                                      size={16}
+                                      className={priorityColors[item]}
+                                    />
+                                )}
+                            <span className={priorityColors[item]}>
+                                {item}
+                            </span>
+                            </div>
+                            {priority === item && <span> <Check size={16} /> </span>}
+
+                        </button>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
